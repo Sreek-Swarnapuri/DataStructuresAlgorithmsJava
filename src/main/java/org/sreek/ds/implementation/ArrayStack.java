22b93f98822a96df261d9beb782570718e4e5b13
Sreekar Swarnapuri <sreekar.swarnapuri@outlook.com>
2024-11-02 13:31:34 +0530

    Stack using arrays - fixed issues where the size/capacity was changing where it should be fixed.

---
+++ src/main/java/org/sreek/ds/implementation/ArrayStack.java
@@ -0,0 +1,88 @@
+package org.sreek.ds.implementation;
+
+import java.util.EmptyStackException;
+
+public class StackUsingArray<T> {
+
+    private final int capacity;
+    private int top;
+    private final T[] elements;
+
+    @SuppressWarnings("unchecked") // as we are maintaining a strict type and control the creation of the array only in this class
+    public StackUsingArray(int size){
+        this.capacity = size;
+        this.top = -1;
+        elements = (T[]) new Object[size];
+    }
+
+    /**
+     * Checks if the stack is empty
+     * @return {@code true} if there are no elements in Stack
+     */
+    public boolean isEmpty() {
+        return this.top == -1;
+    }
+
+    /**
+     * Checks if the stack is full
+     * @return {@code true} if the stack has elements up to provided capacity
+     */
+    public boolean isFull() {
+        return ( this.capacity - 1 ) == this.top;
+    }
+
+    /**
+     * Adds provided data on top of the stack.
+     * @param data data to be pushed onto the stack
+     */
+    public void push(T data) {
+        if( isFull() ) {
+            throw new StackOverflowError("Stack is full");
+        }
+        this.elements[++top] = data;
+    }
+
+
+    /**
+     * Removes the top element of the stack
+     * @return top element of the stack
+     */
+    public T pop() {
+        if( isEmpty() )
+            throw new EmptyStackException();
+        T popElement = this.elements[top--];
+        this.elements[top+1] = null; // Avoid memory leak
+
+        return popElement;
+    }
+
+    /**
+     * Shows the top element of the stack
+     * @return the data on top of the stack
+     */
+    public T peek() {
+        if( isEmpty() )
+            throw new EmptyStackException();
+
+        return this.elements[top];
+    }
+
+    @Override
+    public String toString() {
+        if(isEmpty()) return "Stack is empty!";
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Stack capacity : ").append(capacity).append("\n");
+        sb.append("Stack is as follows: \n");
+
+        for (int i = top; i > -1; i--) {
+            sb.append(i).append(" : ").append(this.elements[i]);
+            if(i == this.top)
+                sb.append("  <--  top");
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/main/java/org/sreek/ds/usage/StackUsingArraysImplUsage.java
@@ -0,0 +1,45 @@
+package org.sreek.ds.usage;
+
+import org.sreek.ds.implementation.ArrayStack;
+
+public class StackUsingArraysImplUsage {
+
+    public static void main(String[] args) {
+        ArrayStack<Integer> stack = new ArrayStack<>(4);
+
+        System.out.println(stack);
+        stack.push(1);
+        System.out.println("Pushing 1 to stack");
+        System.out.println("Stack's top element is " + stack.peek());
+        System.out.println(stack);
+        stack.push(2);
+        System.out.println("Pushing 2 to stack");
+        System.out.println("Stack's top element is " + stack.peek());
+        System.out.println(stack);
+        stack.push(3);
+        System.out.println("Pushing 3 to stack");
+        System.out.println("Stack's top element is " + stack.peek());
+        System.out.println(stack);
+        stack.push(4);
+        System.out.println("Pushing 4 to stack");
+        System.out.println("Stack's top element is " + stack.peek());
+        System.out.println(stack);
+
+        try {
+            System.out.println("Trying to push an element on top of an already full stack: ");
+            stack.push(5);
+        } catch (StackOverflowError e) {
+            System.out.println("Stack is already full");
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Popping stack, top element is: " + stack.pop());
+            System.out.println(stack);
+        }
+
+        System.out.println("Popping stack, top element is: " + stack.pop());
+
+    }
+
+}
